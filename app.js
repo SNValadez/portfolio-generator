4fd1ea0231c0c2573acd18d0a1155bb429b7efe5
SNValadez <snvaladez13@gmail.com>
2021-10-10 15:17:46 -0700

    inquirer npm

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-const fs = require("fs");
+const inquirer = require("inquirer");
 
-const generatePage = require("./src/page-template");
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?"
+    }])
+    .then(answers => console.log(answers));
 
-const profileDataArgs = process.argv.slice(2);
-console.log(profileDataArgs);
+    // const fs = require("fs");
 
-const [name, github] = profileDataArgs;
-console.log(name, github);
+    // const generatePage = require("./src/page-template");
 
-const pageHTML = generatePage(name, github);
+    // const pageHTML = generatePage(name, github);
+
+
     // const printProfileData = profileDataArr => {
     //     // This..
     //     for (let i = 0; i < profileDataArr.length; i += 1) {
@@ -25,8 +32,8 @@ const pageHTML = generatePage(name, github);
 
     
 
-    fs.writeFile("./index.html", pageHTML, err => {
-        if (err) throw err;
+    // fs.writeFile("./index.html", pageHTML, err => {
+    //     if (err) throw err;
 
-        console.log("Portfolio complete! Check out index.html to see the output!")
-    });
+    //     console.log("Portfolio complete! Check out index.html to see the output!")
+    // });
