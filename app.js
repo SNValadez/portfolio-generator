e0318a74e84784e6e43d1bbb1de5a776dc1b26e0
SNValadez <snvaladez13@gmail.com>
2021-10-10 22:31:12 -0700

    app adjustments 2

--- app.js
+++ app.js
@@ -119,12 +119,20 @@ const promptProject = portfolioData => {
             message: "Would you like to enter another project?",
             default: false
         }
-    ]);
+    ])
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+            return promptProject(portfolioData);
+        } else {
+            return portfolioData;
+        }
+    });
 };
 
 promptUser()
     .then(promptProject)
-    .then(projectData => {
+    .then(portfolioData => {
         console.log(portfolioData);
     });
 
